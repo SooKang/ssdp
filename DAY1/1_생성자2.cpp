ab5daf5d0abb2d7415e35a0a5070202ae62e1726
ecourse <smkang@codenuri.co.kr>
2023-03-27 09:40:24 +0900

    Update 1_생성자2.cpp

--- DAY1/1_생성자2.cpp
+++ DAY1/1_생성자2.cpp
@@ -1,4 +1,4 @@
-// 1_������2-1.cpp
+// 1_생성자2-1.cpp
 #include <string>
 #include <iostream>
 
@@ -7,11 +7,25 @@ class People
 	std::string name;
 	int age;
 public:
+	People(const std::string& name, int age)
+		: name(name), age(age) {}
 };
+
+// People 부터 상속받은 "Student" 만들어 보세요
+// => 학번(int id) 추가해 보세요
+// => 생성자로 초기화 해보세요. 
+// => main 함수에서 Student 객체 만들어 보세요
+
+
+
 
 int main()
 {
-	People p1;	// �̷��� ��ü�� ����� �ִٴ� ���� ������� ? (�ʱ�ȭ ���� ���� ��ü)
+//	People p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
+
+	People p1("kim", 30);
 }
+
+
 
 
