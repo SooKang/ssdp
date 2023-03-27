069d25b2e5a788b7cce32726c554796e543e13eb
ecourse <smkang@codenuri.co.kr>
2023-03-27 09:53:40 +0900

    Update 1_생성자3.cpp

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -1,6 +1,12 @@
+// protected 생성자를 만드는 이유
+// => 자신은 객체를 생성할수 없지만( 추상적인 개념 )
+// => 파생 클래스의 객체는 생성할수 있게( 구체적인 개념 )
+
+// 디자인 패턴에서 "추상(abstract)"의 반대말은 "구체(concrete)"입니다.
+
 class Animal
 {
-public:
+protected:
 	Animal() {}
 };
 class Dog : public Animal
@@ -10,9 +16,9 @@ public:
 };
 int main()
 {
-	// ������ ������ ��� ��� ������
-	Animal a;
-	Dog    d;
+	// 다음중 에러를 모두 골라 보세요
+//	Animal a; // error
+	Dog    d; // ok
 }
 
 
