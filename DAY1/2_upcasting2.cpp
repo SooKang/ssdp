c81c6cee697f90fff92472475d6246eb8b2d8727
ecourse <smkang@codenuri.co.kr>
2023-03-27 10:28:06 +0900

    Update 2_upcasting2.cpp

--- DAY1/2_upcasting2.cpp
+++ DAY1/2_upcasting2.cpp
@@ -6,5 +6,8 @@ class Dog : public Animal {};
 
 int main()
 {
-	std::vector<Dog*>    v1;
+	// upcasting 활용 1. 동종을 보관하는 컨테이너 만들기
+	//					 => 동일 기반 클래스를 사용하는 타입.
+	std::vector<Dog*>    v1; // Dog 객체만 보관 가능한 컨테이너
+	std::vector<Animal*> v2; // 모든 동물을 보관하는 컨테이너
 }
