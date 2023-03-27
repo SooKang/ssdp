ea840d7049e0714bbb5122f7dc0dc13e34d7cb45
ecourse <smkang@codenuri.co.kr>
2023-03-27 10:39:48 +0900

    Update 2_upcasting3.cpp

--- DAY1/2_upcasting3.cpp
+++ DAY1/2_upcasting3.cpp
@@ -1,3 +1,4 @@
+// github.com/codenuri/ssdp
 #include <iostream>
 // upcasting3.cpp
 class Animal 
@@ -23,7 +24,15 @@ void NewYear(Animal* p) // 모든 동물을 인자로 받을수있는함수
 	// p가 Dog를 가리키면 color 도 변경
 	// p->color = 10; // error
 
-	Dog* pDog = static_cast<Dog*>(p);
+	// static_cast : 컴파일 시간 캐스팅
+	//				 p가 가리키는 곳이 Dog 인지 조사할수 없다
+	//				 p가 어떤 객체를 가리키는지는 컴파일 시간에 모른다
+	// Dog* pDog = static_cast<Dog*>(p);
+
+	// dynamic_cast : 실행시간 캐스팅
+	//				  실행시간에 p가 가리키는 곳이 Dog인지 조사후 캐스팅
+	//				  Dog가 아니면 0 반환
+	Dog* pDog = dynamic_cast<Dog*>(p);
 
 	std::cout << pDog << std::endl;
 }
