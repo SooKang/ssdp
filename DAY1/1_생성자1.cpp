194c4e7d4b0338bc4d1bc017ddc57dcedda75af2
ecourse <smkang@codenuri.co.kr>
2023-03-27 09:24:20 +0900

    Update 1_생성자1.cpp

--- DAY1/1_생성자1.cpp
+++ DAY1/1_생성자1.cpp
@@ -1,38 +1,35 @@
-// github.com / codenuri / ssdp 에서 "DAY1.zip" 받아두세요
-
 //생성자1.cpp - 3page
 #include <iostream>
 
 class Base
 {
 public:
-	Base() { std::cout << "Base()" << std::endl; }
+	Base()     { std::cout << "Base()" << std::endl; }
 	Base(int a){ std::cout << "Base(int)" << std::endl; }
 	~Base()    { std::cout << "~Base()" << std::endl; }
 };
 class Derived : public Base
 {
 public:						
-	Derived()		{ std::cout << "Derived()" << std::endl; }
-	Derived(int a)	{ std::cout << "Derived(int)" << std::endl; }
-	~Derived()		{ std::cout << "~Derived()" << std::endl; }
+	// 사용자 코드		// 컴파일러가 변경한 코드
+	Derived()			// Derived() : Base() 
+	{ 
+		std::cout << "Derived()" << std::endl; 
+	}
+	Derived(int a)		// Derived(int a) : Base()
+	{ 
+		std::cout << "Derived(int)" << std::endl; 
+	}
+	~Derived()		
+	{ 
+		std::cout << "~Derived()" << std::endl; 
+
+		// ~Base();		// 기반 클래스 소멸자 호출하는 코드를 컴파일러가추가
+	}
 };
 int main()
 {
-//	Derived d1;
-	Derived d2(5);
+//	Derived d1;		// call Derived::Derived()
+	Derived d2(5);	// call Derived::Derived(int)
 
 }
-
-// 1. DAY1.zip 압축 풀러 보세요
-// 
-// 2. DAY1.sln 파일 더블클릭해서 VS 에서 열어 보세요
-
-// 3. 버전 변경 
-//    => 프로젝트 메뉴 선택후
-//    => DAY1 속성 메뉴 선택
-//       ( 플랫폼 도구집합 버전과 Windows SDK 버전 변경)
-
-// 4. 확인 : Control + F5 키 로 빌드
-
-// 5. 소스를 빌드에 포함/제거하는 방법
