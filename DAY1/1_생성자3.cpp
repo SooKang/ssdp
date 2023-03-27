99b07860b5b88d0e1b0a798435ddf5226962ee1b
ecourse <smkang@codenuri.co.kr>
2023-03-27 10:06:09 +0900

    Update 1_생성자3.cpp

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -1,25 +1,48 @@
-// protected 생성자를 만드는 이유
-// => 자신은 객체를 생성할수 없지만( 추상적인 개념 )
-// => 파생 클래스의 객체는 생성할수 있게( 구체적인 개념 )
-
-// 디자인 패턴에서 "추상(abstract)"의 반대말은 "구체(concrete)"입니다.
-
 class Animal
 {
-protected:
-	Animal() {}
+public:
+	int age;
 };
 class Dog : public Animal
 {
 public:
-	Dog() {}
+	int color;
 };
 int main()
 {
-	// 다음중 에러를 모두 골라 보세요
-//	Animal a; // error
-	Dog    d; // ok
+	Dog d;
+
+	Dog* p1 = &d;	// ok
+	
+//	int* p2 = &d;	// error
+	
+	// 핵심 1. 기반 클래스 포인터(참조)로 파생 클래스 객체의 주소를 
+	//         담을수 있다
+	Animal* p3 = &d; // ok. 
+
+
+	// 핵심 2. 하지만 Animal* 로는 Animal 로 부터 상속된 멤버만 사용가능
+	//         Dog 고유 멤버는 접근 안됨
+	// 이유 : 컴파일 시간에 컴파일러는 포인터 타입으로 
+	//        해당 멤버가 있는지 확인하므로
+	//       "static type check"
+	p3->age = 10;   // ok
+	p3->color = 10; // error
+
+
+	// 핵심 3. p3로 Dog 고유 멤버에 접근하려면 캐스팅해야 합니다.
+	// 단, 이경우 p3가 가리키는 곳이 Dog 라는 확신이 있어야 합니다.
+	// 만약 Dog가 아니면 잘못된 메모리 접근으로 runtime error 가능성있음
+	static_cast<Dog*>(p3)->color = 10;
 }
+
+
+
+
+
+
+
+
 
 
 
