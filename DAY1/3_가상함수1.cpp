d38285785bd1aa909823916fd42f291388d85733
ecourse <smkang@codenuri.co.kr>
2023-03-27 10:55:32 +0900

    Update 3_가상함수1.cpp

--- DAY1/3_가상함수1.cpp
+++ DAY1/3_가상함수1.cpp
@@ -3,19 +3,33 @@
 class Animal
 {
 public:
-	void cry() { std::cout << "Animal Cry" << std::endl; } 
+	void cry1() { std::cout << "Animal Cry1" << std::endl; } 
 };
 class Dog : public Animal
 {
 public:
-	void cry() { std::cout << "Dog Cry" << std::endl; } 
+	void cry1() { std::cout << "Dog Cry1" << std::endl; } 
 };
+
 int main()
 {
+	Animal a;
 	Dog d;
+
 	Animal* p = &d; 
 
-	p->cry();
+	if (사용자 입력 == 1) p = &a;
+
+	// 아래 코드를 컴파일 할때 컴파일러는 p가 어떤 객체를 가리키는지
+	// 알수 있을까 ?
+	p->cry1();
 }
+
+
+// p->cry1() 를 어느 함수로 연결할것인가 ? 함수 바인딩 개념
+
+// 1. static binding : 컴파일러가 결정
+
+// 2. dynamic binding : 실행할때 메모리 조사해서 결정
 
 
